89da1c993ffe3ee4dffdd4d04762eb6b227bcc2b
luohoward <bluo4@illinois.edu>
2015-09-14 19:44:41 -0500

    hello

---
+++ cs440_pacman/queue.py
@@ -0,0 +1,13 @@
+class queue(object):
+	# a typical stack with an init, pop and push method
+
+	list m_queue
+
+	def init(self):
+		m_stack = []
+
+	def push(value):
+		m_stack.append(value)
+
+	def pop():
+		m_stack.popleft()
---
+++ cs440_pacman/stack.py
@@ -0,0 +1,13 @@
+class stack(object):
+	# a typical stack with an init, pop and push method
+
+	list m_stack
+
+	def init(self):
+		m_stack = []
+
+	def push(value):
+		m_stack.append(value)
+
+	def pop():
+		m_stack.pop()
