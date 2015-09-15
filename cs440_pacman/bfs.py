cff1e7cbd15d28220b4df52d75f62e05a13fe6f9
luohoward <bluo4@illinois.edu>
2015-09-14 20:58:44 -0500

    swag

--- cs440_pacman/bfs.py
+++ cs440_pacman/bfs.py
@@ -11,20 +11,28 @@ def bfs(maze, start, end, walls):
 		if(currentPoint[0] == end[0] and currentPoint[1] == end[1]):
 			break
 
+		if visited[currentPoint[0], currentPoint[1] - 1] is true and visited[currentPoint[0] + 1, currentPoint[1]] is true and visited[currentPoint[0], currentPoint[1] + 1] is true and visited[currentPoint[0] - 1, currentPoint[1]] is true:
+			steps -= 1
+			visited[currentPoint[0], currentPoint[1]] = true
+			currentPoint = prev
+
 		#implicit else
-		if not visited[currentPoint[0], currentPoint[1] - 1]:
-			prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
-			m_queue.enqueue([currentPoint[0], currentPoint[1] - 1])
+		else:
+			if not visited[currentPoint[0], currentPoint[1] - 1]:
+				prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
+				m_queue.enqueue([currentPoint[0], currentPoint[1] - 1])
 
-		if not visited[currentPoint[0] + 1, currentPoint[1]]:
-			prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
-			m_queue.enqueue([currentPoint[0] + 1, currentPoint[1]])
+			if not visited[currentPoint[0] + 1, currentPoint[1]]:
+				prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
+				m_queue.enqueue([currentPoint[0] + 1, currentPoint[1]])
 
-		if not visited[currentPoint[0], currentPoint[1] + 1]:
-			prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
-			m_queue.enqueue([currentPoint[0], currentPoint[1] + 1])
+			if not visited[currentPoint[0], currentPoint[1] + 1]:
+				prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
+				m_queue.enqueue([currentPoint[0], currentPoint[1] + 1])
 
-		if not visited[currentPoint[0] - 1, currentPoint[1]]:
-			prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
-			m_queue.enqueue([currentPoint[0] - 1, currentPoint[1]])
+			if not visited[currentPoint[0] - 1, currentPoint[1]]:
+				prev[currentPoint[0], currentPoint[1] - 1] = [currentPoint[0], currentPoint[1]]
+				m_queue.enqueue([currentPoint[0] - 1, currentPoint[1]])
 
+			steps += 1
+			visited[currentPoint[0], currentPoint[1]] = true
