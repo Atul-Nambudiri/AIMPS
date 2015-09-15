44f0498cf43fca2c22c5f8740c49119ed8f62dc2
luohoward <bluo4@illinois.edu>
2015-09-14 21:17:35 -0500

    hello

--- cs440_pacman/queue.py
+++ cs440_pacman/queue.py
@@ -1,16 +1,14 @@
 class queue(object):
 	# a typical queue with an init, enqueue and dequeue method
 
-	list m_queue = []
-
-	def init(self):
+	def __init__(self):
 		self.m_queue = []
 
 	def enqueue(self, value):
 		self.m_queue.append(value)
 
 	def dequeue(self):
-		self.m_queue.popleft()
+		return self.m_queue.popleft()
 
 	def isEmpty(self):
 		return self.m_queue == 0;
