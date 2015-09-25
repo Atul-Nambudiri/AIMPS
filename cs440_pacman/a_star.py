3c5e6b02cc8550a6b6f43bfdfb07d3b33810ce26
Atul-Nambudiri <atul.nambudiri@gmail.com>
2015-09-25 14:36:12 -0500

    Added in some comments

--- cs440_pacman/a_star.py
+++ cs440_pacman/a_star.py
@@ -5,7 +5,7 @@ from Queue import PriorityQueue
 
 def manhattan_distance(current, dest):
 	"""
-	This function calculates the manhattan distance between teo points current and dest
+	This function calculates the manhattan distance between two points current and dest
 	"""
 	return math.fabs(current[0] - dest[0]) + math.fabs(current[1] - dest[1])
 
@@ -16,19 +16,19 @@ def a_star(maze, start, end, walls):
 	opened = 0
 	p_queue = PriorityQueue(maxsize=0)			#Use a priority queue to get the position with the highest priority
 
-	cost_so_far = {}
-	cost = {}
-	prev = {}
+	cost_so_far = {}					#This dictionary keep track of the number of steps taken to reach a particular point
+	cost = {}						#This dictionary keeps track of the total cost, IE cost_so_far + manhattan distance to end, for each point
+	prev = {}						#This dictionary keeps track fo the previous for a particular point
 
-	start_pos = Position(start, 0)
+	start_pos = Position(start, 0)				#Add the start to the priority queue
 	p_queue.put(start_pos)
 	prev[start] = None
 	cost_so_far[start] = 0
 	cost[start] = 0
 
 	while not p_queue.empty():
-		opened += 1	
-		current = p_queue.get()
+		opened += 1									
+		current = p_queue.get()				#Remove an item from the priority queue
 		x_pos = current.pos[0]
 		y_pos = current.pos[1]
  
@@ -37,19 +37,19 @@ def a_star(maze, start, end, walls):
 
 		neighbors = [(x_pos -1, y_pos), (x_pos, y_pos + 1), (x_pos + 1, y_pos), (x_pos, y_pos - 1)]
 
-		for neighbor in neighbors:
-			if not neighbor[0] < 0 and not neighbor[1] < 0 and not neighbor[0] >= len(walls) and not neighbor[1] >= len(walls[0]):
-				if not walls[neighbor[0]][neighbor[1]]:
-					if neighbor not in cost or cost[neighbor] > (cost_so_far[(x_pos, y_pos)] + 1 + manhattan_distance(neighbor, end)):
-						new = Position(neighbor, cost_so_far[(x_pos, y_pos)] + 1 + manhattan_distance(neighbor, end))
-						cost_so_far[new.pos] = cost_so_far[(x_pos, y_pos)] + 1
+		for neighbor in neighbors:			#Consider each of the four neighbors of the current point
+			if not neighbor[0] < 0 and not neighbor[1] < 0 and not neighbor[0] >= len(walls) and not neighbor[1] >= len(walls[0]):		#Cant go out of bounds of the maze
+				if not walls[neighbor[0]][neighbor[1]]:		#Can't go into a wall
+					if neighbor not in cost or cost[neighbor] > (cost_so_far[(x_pos, y_pos)] + 1 + manhattan_distance(neighbor, end)):	#Make sure you either havn't looked at a position yet, or the cost to get to that position by following the current path is less than by following the path that is currently set for that positon
+						new = Position(neighbor, cost_so_far[(x_pos, y_pos)] + 1 + manhattan_distance(neighbor, end))		#Create a new Position object for the neighbor
+						cost_so_far[new.pos] = cost_so_far[(x_pos, y_pos)] + 1	
 						cost[new.pos] = cost_so_far[(x_pos, y_pos)] + 1 + manhattan_distance(neighbor, end)
 						prev[new.pos] = [x_pos, y_pos]
-						p_queue.put(new)
+						p_queue.put(new)			#Put the neighbor in the queue
 
 	path = copy.deepcopy(maze)
 	current = end
-	while maze[current[0]][current[1]] != 'P':
+	while maze[current[0]][current[1]] != 'P':				#Generate the path from the start to then end, and write it on the maze list
 		current = prev[(current[0], current[1])]
 		path[current[0]][current[1]] = '.'
 
--- cs440_pacman/a_star12.py
+++ cs440_pacman/a_star12.py
@@ -4,43 +4,61 @@ from position import Position
 from Queue import PriorityQueue
 
 def manhattan_distance(current, dest, straight, turn, direction_facing):
+	"""
+	This function calculates the manhattan distance from the current point to the destination
+	"""
 	return math.fabs(current[0] - dest[0]) + math.fabs(current[1] - dest[1])
 
-def distance(current, dest, straight, turn, direction_facing):
-	return math.sqrt(math.pow(math.fabs(current[0] - dest[0]), 2) + math.pow(math.fabs(current[1] - dest[1]), 2))
-
 def new(current, dest, straight, turn, direction_facing):
-	change_x = math.fabs(current[0] - dest[0])
+	"""
+	This function calculates the new heuristic distance from the current point to the destination. It is an extension of the manhattan distance. However, the varied costs for turns and going straight are taken into account. We calculate the minimum number of turns that are required to reach the end of the maze, and the minmum number of times we have to go straight. We then mulitply both of these values by their respective multipliers. We then return this value.
+	"""
+	change_x = math.fabs(current[0] - dest[0])		#Calculate the total number of times we have to go straight
 	change_y =  math.fabs(current[1] - dest[1])
 	num_turns = 0
-	if direction_facing == 1:
+	if direction_facing == 1:				#Calculate the number of turns we have to take when facing different directions
 		if dest[0] - current[0] > 0:
 			num_turns +=  2
+		if dest[1] != current[1]:
+			num_turns += 1
 	if direction_facing == 3:
 		if dest[0] - current[0] < 0:
 			num_turns +=  2
+		if dest[1] != current[1]:
+			num_turns += 1
 	if direction_facing == 2:
 		if dest[1] - current[1] < 0:
 			num_turns +=  2
+		if dest[1] != current[1]:
+			num_turns += 1
 	if direction_facing == 4:
 		if dest[1] - current[1] > 0:
 			num_turns +=  2
+		if dest[1] != current[1]:
+			num_turns += 1
 
-	return (change_x + change_y) * straight + turn * num_turns
+	return (change_x + change_y) * straight + turn * num_turns	#Multiply by the costs
 
 def a_star12(maze, start, end, walls):
+	"""
+	This returns an array with the results of runnning a_star with the two heurstics, with the two different sets of costs
+	"""
 	return [a_star(maze, start, end , walls, manhattan_distance, 1, 2), a_star(maze, start, end , walls, manhattan_distance, 2, 1), a_star(maze, start, end , walls, new, 1, 2), a_star(maze, start, end , walls, new, 2, 1)]
 
 def a_star(maze, start, end, walls, distance_function, straight, turn):
+	"""
+	Runs a_star with the passed in distance formula, and costs for going straight and turning
+	"""
 	opened = 0
-	p_queue = PriorityQueue(maxsize=0)
+	p_queue = PriorityQueue(maxsize=0)			#Use a priority queue to get the position with the highest priority
 
-	cost_so_far = {}
-	cost = {}
-	prev = {}
-	direction_facing = {}
+	cost_so_far = {}					#This dictionary keeps track of the cost of the total number of steps taken to reach a particular point
+	cost = {}						#This dicitonary keeps track of the total cost, IE cost_so_far + distance to end, for each point 
 
-	start_pos = Position(start, 0)
+	prev = {}						#This dictionary keeps track of the previous for a particular point
+	direction_facing = {}					#This dictionary keeps track of th edirection that you are facing at any point. 
+	
+	start_pos = Position(start, 0)				#Add start to the priority queue
 	p_queue.put(start_pos)
 	prev[start] = None
 	cost_so_far[start] = 0
@@ -49,7 +67,7 @@ def a_star(maze, start, end, walls, distance_function, straight, turn):
 
 	while not p_queue.empty():
 		opened += 1
-		current = p_queue.get()
+		current = p_queue.get()				#Remove the item with the highest priority from the queue
 		x_pos = current.pos[0]
 		y_pos = current.pos[1]
 		cur_dir = direction_facing[current.pos]
@@ -57,29 +75,29 @@ def a_star(maze, start, end, walls, distance_function, straight, turn):
 		if x_pos == end[0] and y_pos == end[1]:
 			break
 
-		neighbors = {(x_pos -1, y_pos) : 1, (x_pos, y_pos + 1) : 2, (x_pos + 1, y_pos) : 3, (x_pos, y_pos - 1) : 4}
+		neighbors = {(x_pos -1, y_pos) : 1, (x_pos, y_pos + 1) : 2, (x_pos + 1, y_pos) : 3, (x_pos, y_pos - 1) : 4}			#Keeps track of all the four neighbors from the current point, along witht he direction you would be facing if you go to that point
 
-		for neighbor, new_dir in neighbors.iteritems():
-			if not neighbor[0] < 0 and not neighbor[1] < 0 and not neighbor[0] >= len(walls) and not neighbor[1] >= len(walls[0]):
-				if not walls[neighbor[0]][neighbor[1]]:
-					extra_cost = 0
-					if new_dir == cur_dir:
+		for neighbor, new_dir in neighbors.iteritems():		#Consider each of the four neighbors
+			if not neighbor[0] < 0 and not neighbor[1] < 0 and not neighbor[0] >= len(walls) and not neighbor[1] >= len(walls[0]):			#Can't go out of bounds of the maze
+				if not walls[neighbor[0]][neighbor[1]]:		#Can't go into a wall
+					extra_cost = 0		#This calculates th cost of moving in the new direction, based upon the passed in costs for going straight and turning, and the number of turns needed to move in a new direction
+					if new_dir == cur_dir:	
 						extra_cost = straight
 					elif math.fabs(new_dir - cur_dir) % 2 == 0:
 						extra_cost = 2 * turn + straight
 					else:
 						extra_cost = straight + turn
-					if neighbor not in cost or cost[neighbor] > (cost_so_far[(x_pos, y_pos)] + extra_cost + distance_function(neighbor, end, straight, turn, new_dir)):
-						new = Position(neighbor, cost_so_far[(x_pos, y_pos)] + extra_cost + distance_function(neighbor, end, straight, turn, new_dir))
+					if neighbor not in cost or cost[neighbor] > (cost_so_far[(x_pos, y_pos)] + extra_cost + distance_function(neighbor, end, straight, turn, new_dir)): #Make sure you either haven't looked at a position yet, or the cost to get to that position by followign the current path is less then by following the path that is currently set for that position
+						new = Position(neighbor, cost_so_far[(x_pos, y_pos)] + extra_cost + distance_function(neighbor, end, straight, turn, new_dir))		#Create a new position object for the neighbor.
 						cost_so_far[new.pos] = cost_so_far[(x_pos, y_pos)] + extra_cost
 						cost[new.pos] = cost_so_far[(x_pos, y_pos)] + extra_cost + distance_function(neighbor, end, straight, turn, new_dir)
 						prev[new.pos] = [x_pos, y_pos]
-						p_queue.put(new)
+						p_queue.put(new)	#Put the neighbor in the p_queue
 						direction_facing[neighbor] = new_dir
 
 	path = copy.deepcopy(maze)
 	current = end
-	while maze[current[0]][current[1]] != 'P':
+	while maze[current[0]][current[1]] != 'P':		#Generate the path from the start to the end, and write it on the maze list
 		current = prev[(current[0], current[1])]
 		path[current[0]][current[1]] = '.'
 
