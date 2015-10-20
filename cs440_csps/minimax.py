8736ea3e5f96d405be15ee191c99c3f70ac9bdc1
Atul-Nambudiri <atul.nambudiri@gmail.com>
2015-10-19 22:54:26 -0500

    Added in board

--- cs440_csps/minimax.py
+++ cs440_csps/minimax.py
@@ -1,1 +1,12 @@
-Swag
+import sys 
+
+def main():
+	if len(sys.argv) < 2:
+		print("Must Enter in a Map")
+	else:
+		board = []
+		with open(sys.argv[1], 'r') as board_file:
+			for line in board_file:
+				linelist = line.split(" ")
+				
+
